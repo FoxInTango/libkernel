cbcf851e42aca6ac36ad75d79acbabc9e7f3972d
Jack.WSL <foxintango@yeah.net>
2023-05-20 21:58:52 +0800

    alpine

--- modules/kernel_syscall_hooks/src/module.c
+++ modules/kernel_syscall_hooks/src/module.c
@@ -11,7 +11,7 @@ MODULE_LICENSE("GPL");
 
 static int __init this_module_init(void)          /* 加载模块时自动执行 */
 {
-    echo("Hello Module inited.\n");
+    echo("kernel_syscall_hooks inited.\n");
     cpp_on_init();
     return 0;
 }
@@ -19,9 +19,8 @@ static int __init this_module_init(void)          /* 加载模块时自动执行
 static void __exit this_module_exit(void)
 {
     cpp_on_exit();
-    echo("Hello Module exited.\n");
+    echo("kernel_syscall_hooks exited.\n");
 }
-
 
 module_init(this_module_init);
 module_exit(this_module_exit);
