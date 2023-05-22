1229706e3205e917188b91a0767810185b7c4876
Jack.WSL <foxintango@yeah.net>
2023-05-22 20:00:53 +0800

    alpine

---
+++ modules/kernel_filesystem_sm/src/module.c
@@ -0,0 +1,32 @@
+#include <linux/module.h>
+#include <linux/init.h>
+#include <linux/moduleparam.h>
+#include <linux/kallsyms.h>
+#include "hook.h"
+#include "../../core/echo.h"
+
+MODULE_AUTHOR("Dali Li");
+MODULE_LICENSE("GPL");
+
+//module_param(nbr,int,S_IRUGO);
+
+static int __init this_module_init(void)          /* 加载模块时自动执行 */
+{
+    echo("kernel_syscall_hooks inited.\n");
+    unsigned long* sys_call_table;
+
+    // 获取 sys_call_table 的虚拟内存地址
+    sys_call_table = (unsigned long*)kallsyms_lookup_name("sys_call_table");
+    echo("sys_call_table address %p\n", sys_call_table);
+    cpp_on_init();
+    return 0;
+}
+
+static void __exit this_module_exit(void)
+{
+    cpp_on_exit();
+    echo("kernel_syscall_hooks exited.\n");
+}
+
+module_init(this_module_init);
+module_exit(this_module_exit);
---
+++ modules/kernel_network_hooks/src/hook.h
@@ -0,0 +1,15 @@
+#ifndef _HOOK_H_
+#define _HOOK_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void cpp_on_init(void);
+void cpp_on_exit(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
