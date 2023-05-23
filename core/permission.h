f18dd2d5658ff6b42dada46e78706c052e6ad257
Jack.WSL <foxintango@yeah.net>
2023-05-23 16:59:16 +0800

    alpine

---
+++ core/permission.h
@@ -0,0 +1,7 @@
+#ifndef _PERMISSION_H_foxintango
+#define _PERMISSION_H_foxintango
+
+typedef struct _filesystem_permission{}filesystem_permission;
+typedef struct _network_permission{}network_permission;
+
+#endif
