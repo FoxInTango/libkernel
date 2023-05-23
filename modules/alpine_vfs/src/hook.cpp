e159d9b22f3c0006b091feec5a224a676c827e45
Jack.WSL <foxintango@yeah.net>
2023-05-23 17:39:18 +0800

    alpine

--- modules/alpine_vfs/src/hook.cpp
+++ modules/alpine_vfs/src/hook.cpp
@@ -1,9 +1,29 @@
 #include "hook.h"
 #include "../../core/echo.h"
 
-#include <vector>
 #include <libarguments/libarguments.h>
 #include <libcpp/libcpp.h>
+
+#include <linux/module.h>
+#include <linux/string.h>
+#include <linux/fs.h>
+#include <linux/time.h>
+#include <linux/slab.h>
+#include <linux/init.h>
+#include <linux/blkdev.h>
+#include <linux/parser.h>
+#include <linux/smp_lock.h>
+#include <linux/buffer_head.h>
+#include <linux/exportfs.h>
+#include <linux/vfs.h>
+#include <linux/random.h>
+#include <linux/mount.h>
+#include <linux/namei.h>
+#include <linux/quotaops.h>
+#include <linux/seq_file.h>
+#include <asm/uaccess.h>
+
+#include <vector>
 class Hook {
 public:
     //std::vector<int> hook_array;
