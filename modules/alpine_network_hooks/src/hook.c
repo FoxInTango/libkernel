0f00cc63ec16660d0afd70395271eb8b1b2a5d4d
Jack.WSL <foxintango@yeah.net>
2023-05-24 20:42:41 +0800

    alpine

--- modules/alpine_network_hooks/src/hook.c
+++ modules/alpine_network_hooks/src/hook.c
@@ -10,8 +10,9 @@
 #include <linux/tcp.h>
 #include <linux/netfilter.h>
 #include <linux/netfilter_ipv4.h>
+//unsigned int alpine_ipv4_in(unsigned int hooknum, struct sk_buff* skb, const struct net_device* in, const struct net_device* out, int(*okfn)(struct sk_buff*))
 
-unsigned int alpine_ipv4_in(unsigned int hooknum, struct sk_buff* skb, const struct net_device* in, const struct net_device* out, int(*okfn)(struct sk_buff*))
+unsigned int alpine_ipv4_in(void*, struct sk_buff*, const struct nf_hook_state*)
 {
     echo("alpine ipv4 hooking.\n");
     return NF_ACCEPT;
@@ -57,10 +58,22 @@ static struct nf_hook_ops alpine_network_hooks[] = {
 }
 
 int alpine_network_hook_startup(void){
-    return nf_register_net_hooks(alpine_network_hooks,ARRAY_SIZE(alpine_network_hooks));
+    return ( 
+        nf_register_net_hook(alpine_network_hooks[0]) &&
+        nf_register_net_hook(alpine_network_hooks[1]) &&
+        nf_register_net_hook(alpine_network_hooks[2]) &&
+        nf_register_net_hook(alpine_network_hooks[3]) &&
+        nf_register_net_hook(alpine_network_hooks[4]) &&
+        nf_register_net_hook(alpine_network_hooks[5])
+        ) ? 1:0;
 }
 void alpine_network_hook_shutdown(void){
-    nf_unregister_net_hooks(alpine_network_hooks, ARRAY_SIZE(alpine_network_hooks));
+    nf_unregister_net_hook(alpine_network_hooks[0]);
+    nf_unregister_net_hook(alpine_network_hooks[1]);
+    nf_unregister_net_hook(alpine_network_hooks[2]);
+    nf_unregister_net_hook(alpine_network_hooks[3]);
+    nf_unregister_net_hook(alpine_network_hooks[4]);
+    nf_unregister_net_hook(alpine_network_hooks[5]);
 }
 
 // 钩子返回值 https://blog.csdn.net/weixin_41400449/article/details/106764232
