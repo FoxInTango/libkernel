e707878c523732f5f91a1ea50af620e1e23736d7
Jack.WSL <foxintango@yeah.net>
2023-05-24 17:23:32 +0800

    alpine

--- modules/alpine_vfs/src/vfs.c
+++ modules/alpine_vfs/src/vfs.c
@@ -6,23 +6,23 @@
 #include <linux/moduleparam.h>
 #include <linux/kallsyms.h>
 
-unsigned int alpine_vfs_open(){ return 0;}
+unsigned int alpine_vfs_open(void){ return 0;}
 EXPORT_SYMBOL(alpine_vfs_open);
-unsigned int alpine_vfs_seek() { return 0; }
+unsigned int alpine_vfs_seek(void) { return 0; }
 EXPORT_SYMBOL(alpine_vfs_seek);
-unsigned int alpine_vfs_read() { return 0; }
+unsigned int alpine_vfs_read(void) { return 0; }
 EXPORT_SYMBOL(alpine_vfs_read);
-unsigned int alpine_vfs_write() { return 0; }
+unsigned int alpine_vfs_write(void) { return 0; }
 EXPORT_SYMBOL(alpine_vfs_write);
 
-int alpine_vfs_mkdir(){ return 0; }
+int alpine_vfs_mkdir(void){ return 0; }
 EXPORT_SYMBOL(alpine_vfs_mkdir);
-int alpine_vfs_rmdir(){ return 0; }
+int alpine_vfs_rmdir(void){ return 0; }
 EXPORT_SYMBOL(alpine_vfs_rmdir);
 
-int alpine_vfs_mklink(){ return 0; }
+int alpine_vfs_mklink(void){ return 0; }
 EXPORT_SYMBOL(alpine_vfs_mklink);
-int alpine_vfs_rmlink(){ return 0; }
+int alpine_vfs_rmlink(void){ return 0; }
 EXPORT_SYMBOL(alpine_vfs_rmlink);
 
 int alpine_vfs_init(void) {
