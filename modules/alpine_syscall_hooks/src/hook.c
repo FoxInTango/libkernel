139e11b8123ceddc5e058ec2f8226bce41efac59
Jack.WSL <foxintango@yeah.net>
2023-05-25 18:00:21 +0800

    alpine

--- modules/alpine_syscall_hooks/src/hook.c
+++ modules/alpine_syscall_hooks/src/hook.c
@@ -45,14 +45,14 @@ static long unsigned int original_syscall_table[512];
 typedef 
 ssize_t (*ksys_read_func)(unsigned int fd, char __user* buf, size_t count);
 
-long unsigned int* lookup_syscall_table(){
+long unsigned int* lookup_syscall_table(void){
     return (long unsigned int*)kallsyms_lookup_name("sys_call_table");
 }
 
-void make_syscall_table_rw(){}
-void make_syscall_table_ro(){}
+void make_syscall_table_rw(void){}
+void make_syscall_table_ro(void){}
 
-long unsigned int replace_syscall_item(unsigned int index, long unsigned int value){}
+long unsigned int replace_syscall_item(unsigned int index, long unsigned int value){ return 0; }
 
 ssize_t alpine_ksys_read(unsigned int fd, char __user* buf, size_t count){
     echo("alpine_ksys_read.\n");
@@ -65,7 +65,7 @@ int install_hooks(void) {
     echo("sys_call_table address %p\n", sys_call_table);
     make_vm_rw((long unsigned int)sys_call_table);
     original_syscall_table[__NR_read] = sys_call_table[__NR_read];
-    sys_call_table[__NR_read] = alpine_ksys_read;
+    sys_call_table[__NR_read] = (long unsigned int)alpine_ksys_read;
     make_vm_ro((long unsigned int)sys_call_table);
     return 0;
 }
