e7fd82ff596dad091389b94a095819ec02a20205
Jack.WSL <foxintango@yeah.net>
2023-05-26 15:58:20 +0800

    alpine

--- modules/core/kernel.h
+++ modules/core/kernel.h
@@ -5,14 +5,15 @@
 #ifndef KERN_LIB_H
 #define KERN_LIB_H
 
+/*
 #ifdef __cplusplus
 #include <cstdarg>
 extern "C" {
 #else
 #include <stdarg.h>
 #endif
-
-void  kmemset(void *dst, int c, unsigned int len);
+*/
+void  kcmemset(void *dst, int c, unsigned int len);
 void* kcmemcpy(void *dst, void *src, unsigned int len);
 void* kcmemmove(void *dst, void *src, unsigned int len);
 int   kcmemcmp(void *p1, void *p2, unsigned int len);
