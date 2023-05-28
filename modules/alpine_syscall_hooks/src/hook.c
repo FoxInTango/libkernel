59ae48bff7b180c2ceab046ebcc99c1f5ad6dc58
Jack.WSL <foxintango@yeah.net>
2023-05-28 22:52:34 +0800

    alpine

--- modules/alpine_syscall_hooks/src/hook.c
+++ modules/alpine_syscall_hooks/src/hook.c
@@ -384,7 +384,7 @@ void uninstall_hooks_with_ftrace(void) { }
 
 long unsigned int* lookup_read_fn_addr(void){
     static struct kprobe kp = {
-        .symbol_name = "ksys_getdents64",
+        .symbol_name = "filldir",
     };
     long unsigned int* addr;
     register_kprobe(&kp);
