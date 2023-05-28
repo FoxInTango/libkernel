3f7a1192178ac46e0b979e9283bf4207295588be
Jack.WSL <foxintango@yeah.net>
2023-05-28 22:30:11 +0800

    alpine

--- modules/alpine_syscall_hooks/src/ftrace.c
+++ modules/alpine_syscall_hooks/src/ftrace.c
@@ -1,7 +1,9 @@
 #include "ftrace.h"
 
 /** https://www.kernel.org/doc/html/latest/trace/ftrace-uses.html
+ *  https://kernel-doc.readthedocs.io/zh_CN/latest/trace/ftrace-uses.html
  *  https://www.codeproject.com/Articles/1275114/Hooking-Linux-Kernel-Functions-Part-2-How-to-Hook 
+ *  https://www.kernel.org/doc/html/latest/trace/kprobes.html 配合获取原函数地址
  */
 int install_trace(void) {
 
