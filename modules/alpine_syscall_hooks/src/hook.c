d9c7b1bae05d386b4e944cd0bfefa571c489b321
Jack.WSL <foxintango@yeah.net>
2023-05-28 22:36:54 +0800

    alpine

--- modules/alpine_syscall_hooks/src/hook.c
+++ modules/alpine_syscall_hooks/src/hook.c
@@ -384,7 +384,7 @@ void uninstall_hooks_with_ftrace(void) { }
 
 long unsigned int* lookup_read_fn_addr(void){
     static struct kprobe kp = {
-        .symbol_name = "open",
+        .symbol_name = "read",
     };
     long unsigned int* addr;
     register_kprobe(&kp);
