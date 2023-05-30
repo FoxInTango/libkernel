507eafa1e5378710ebe09a196fe204baee3d02e6
Jack.WSL <foxintango@yeah.net>
2023-05-31 00:05:32 +0800

    alpine

--- modules/alpine_syscall_hooks/src/hook.c
+++ modules/alpine_syscall_hooks/src/hook.c
@@ -5,10 +5,3 @@
  *  https://www.codeproject.com/Articles/1275114/Hooking-Linux-Kernel-Functions-Part-2-How-to-Hook 
  *  https://www.kernel.org/doc/html/latest/trace/kprobes.html 配合获取原函数地址
  */
-int install_trace(void) {
-
-}
-void uninstall_trace(void)
-{
-
-}
